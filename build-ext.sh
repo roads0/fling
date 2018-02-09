d1d92dcf3abe2cc7cf188c652997cd20c8eee8be
SplitPixl <splitpixl@gmail.com>
2018-02-08 16:52:59 -0800

    Start fresh

--- build-ext.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -rf static chrome-ext
-echo "const fs=require('fs'),pug=require('pug');fs.writeFileSync('chrome-ext/index.html', pug.renderFile('views/index.pug'))" > temp-build.js
-node temp-build.js
-rm temp-build.js
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const config = require('../config');
-const index = express.Router();
-
-index.get('/', (req, res) => {
-  res.render('index', {user: req.user || {}});
-});
-
-module.exports = index
